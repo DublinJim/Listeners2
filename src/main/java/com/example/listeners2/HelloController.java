529db1e8789b19374061ebbc1bf88163bd9688fd
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-05 15:17:56 -0500

    init commit2

--- src/main/java/com/example/listeners2/HelloController.java
+++ src/main/java/com/example/listeners2/HelloController.java
@@ -1,7 +1,5 @@
 package com.example.listeners2;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -13,8 +11,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-public HelloApplication helloApplication = new HelloApplication();
-public Stage hStage;
     public CheckBox chkbx1;
     public Button btn1;
     public Text txt1;
@@ -25,14 +21,11 @@ public Stage hStage;
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
 
-        chkbx1.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
-                txt1.setText(String.valueOf(chkbx1.isSelected()));
+        chkbx1.selectedProperty().addListener((observableValue, aBoolean, t1) -> {
+            txt1.setText(String.valueOf(chkbx1.isSelected()));
 
-                Stage stage = (Stage) RootPane.getScene().getWindow();
-                stage.setAlwaysOnTop(chkbx1.isSelected());
-            }
+            Stage stage = (Stage) RootPane.getScene().getWindow();
+            stage.setAlwaysOnTop(chkbx1.isSelected());
         });
         txt1.setText("Ready");
 
